0422887cf2b96c3f2b1df6e2a9929f1f0df0bd2b
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2019-12-22 18:50:25 +0530

    Update employees hired in last n months.sql

--- employees hired in last n months.sql
+++ employees hired in last n months.sql
@@ -4,3 +4,16 @@ select *, DATEDIFF(MONTH, HireDate, GETDATE()) as DIFF
 from Employees
 where DATEDIFF(MONTH, HireDate, GETDATE()) Between 1 and 3
 order by HireDate
+
+
+--All Employees hired in Last 30 days
+select *, DATEDIFF(DAY, HireDate, GETDATE()) as DIFF
+from Employees
+where DATEDIFF(DAY, HireDate, GETDATE()) Between 1 and 30
+order by HireDate
+
+--All Employees hired in last 1 Years
+select *, DATEDIFF(YEAR, HireDate, GETDATE()) as DIFF
+from Employees
+where DATEDIFF(YEAR, HireDate, GETDATE()) Between 1 and 1
+order by HireDate
