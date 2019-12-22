acc5961b3a9b283ec10470038d8e11ef172a1bc1
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2019-12-22 11:16:09 +0530

    Create Query to get organization hierarchy.sql

---
+++ Query to get organization hierarchy.sql
@@ -0,0 +1,36 @@
+Here is the problem definition:
+1. Employees table contains the following columns 
+    a) EmployeeId, 
+    b) EmployeeName 
+    c) ManagerId 
+2. If an EmployeeId is passed, the query should list down the entire organization hierarchy i.e who is the manager of the EmployeeId passed and who is managers manager and so on till full hierarchy is listed.
+
+For example, 
+Scenario 1: If we pass David's EmployeeId to the query, then it should display the organization hierarchy starting from David.
+
+Scenario 2: If we pass Lara's EmployeeId to the query, then it should display the organization hierarchy starting from Lara.
+
+
+--QUERY : 
+
+Declare @ID int;
+Set @ID = 7;
+
+With EmployeeCTE AS
+(
+	Select EmployeeID, EmployeeName, ManagerID
+	From Employees
+	Where EmployeeID = @ID
+
+	UNION ALL
+	
+	Select Employees.EmployeeID, Employees.EmployeeName, Employees.ManagerID
+	From Employees
+	Join EmployeeCTE
+	ON Employees.EmployeeID =EmployeeCTE.ManagerID
+)
+
+select E1.EmployeeName, ISNULL(E2.EmployeeName, 'No Boss') as ManagerName
+from EmployeeCTE E1
+Left Join EmployeeCTE E2
+ON E1.ManagerID = E2.EmployeeID
