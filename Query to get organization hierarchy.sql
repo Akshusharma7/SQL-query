4d235b039170061ea399ea9233138fe64ecdb104
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-02-09 23:27:11 +0530

    Update Query to get organization hierarchy.sql

--- Query to get organization hierarchy.sql
+++ Query to get organization hierarchy.sql
@@ -7,7 +7,6 @@
 --    c) ManagerId 
 --2. If an EmployeeId is passed, the query should list down the entire organization hierarchy i.e who is the manager of the EmployeeId passed and who is managers manager and so on till full hierarchy is listed.
 
-
 --For example, 
 --Scenario 1: If we pass David's EmployeeId to the query, then it should display the organization hierarchy starting from David.
 --Scenario 2: If we pass Lara's EmployeeId to the query, then it should display the organization hierarchy starting from Lara.
